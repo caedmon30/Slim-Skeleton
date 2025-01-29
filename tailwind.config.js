0bc2e84369c175964d964ea209b0b014744a5d8f
Caedmon Walters <cwalters@umd.edu>
2025-01-28 22:04:31 -0500

    added theme colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,17 @@ module.exports = {
             'xl': '1280px',
             '2xl': '1536px',
         },
+        colors: {
+            'blue': '#1fb6ff',
+            'purple': '#7e5bef',
+            'pink': '#ff49db',
+            'orange': '#ff7849',
+            'green': '#13ce66',
+            'yellow': '#ffc82c',
+            'gray-dark': '#273444',
+            'gray': '#8492a6',
+            'gray-light': '#d3dce6',
+        },
         extend: {
             fontFamily: {
                 sans: ['InterVariable', ...fontFamily.sans],
