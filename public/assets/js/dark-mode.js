3e68671376f2ab41d219a716aa69aaf1b86ab05e
Caedmon Walters <cwalters@umd.edu>
2025-02-02 08:36:25 -0500

    devextreme darkmode RC

--- public/assets/js/dark-mode.js
+++ public/assets/js/dark-mode.js
@@ -1,9 +1,12 @@
 const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
 const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+const mainApp = document.getElementById('main-app');
 
 // Change the icons inside the button based on previous settings
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     themeToggleLightIcon.classList.remove('hidden');
+    mainApp.classList.remove('dx-swatch-umd-fluent-scheme-light');
+    mainApp.classList.add('dx-swatch-umd-fluent-scheme-dark');
 } else {
     themeToggleDarkIcon.classList.remove('hidden');
 }
@@ -23,14 +26,12 @@ themeToggleBtn.addEventListener('click', function () {
         if (localStorage.getItem('color-theme') === 'light') {
             document.documentElement.classList.add('dark');
             localStorage.setItem('color-theme', 'dark');
-            localStorage.setItem("dx-theme", 'material.orange.dark');
-            window.location.reload();
+            $("#main-app").addClass('dx-swatch-umd-fluent-scheme-dark').removeClass('dx-swatch-umd-fluent-scheme-light')
         } else {
             document.documentElement.classList.remove('dark');
             localStorage.setItem('color-theme', 'light');
-            localStorage.setItem("dx-theme", 'material.orange.light');
             localStorage.setItem('prefers-color-scheme', 'light');
-            window.location.reload();
+            $("#main-app").addClass('dx-swatch-umd-fluent-scheme-light').removeClass('dx-swatch-umd-fluent-scheme-dark')
         }
 
         // if NOT set via local storage previously
@@ -38,14 +39,13 @@ themeToggleBtn.addEventListener('click', function () {
         if (document.documentElement.classList.contains('dark')) {
             document.documentElement.classList.remove('dark');
             localStorage.setItem('color-theme', 'light');
-            localStorage.setItem("dx-theme", 'material.orange.light');
             localStorage.setItem('prefers-color-scheme', 'light');
-            window.location.reload();
+            $("#main-app").addClass('dx-swatch-umd-fluent-scheme-light').removeClass('dx-swatch-umd-fluent-scheme-dark')
         } else {
             document.documentElement.classList.add('dark');
             localStorage.setItem('color-theme', 'dark');
-            localStorage.setItem("dx-theme", 'material.orange.dark');
-            window.location.reload();
+            $("#main-app").addClass('dx-swatch-umd-fluent-scheme-dark').removeClass('dx-swatch-umd-fluent-scheme-light')
+
         }
     }
 
