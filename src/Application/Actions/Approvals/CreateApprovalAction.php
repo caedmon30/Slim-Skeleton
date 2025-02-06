785614d58e0206f8a6fd7101ffe9b37d5093ce6b
Caedmon Walters <cwalters@umd.edu>
2025-02-06 15:12:36 -0500

    create approval actions

--- src/Application/Actions/Approvals/CreateApprovalAction.php
+++ src/Application/Actions/Approvals/CreateApprovalAction.php
@@ -14,8 +14,6 @@ class CreateApprovalAction extends ApprovalAction
     protected function action(): Response
     {
         $data = $this->getFormData();
-        $data['submitted_by'] = 'cwalters';
-        $data['card_access'] = serialize($data['card_access']);
         $approval = $this->approvalRepository->createApproval($data);
         $this->logger->info("New approval created!");
 
