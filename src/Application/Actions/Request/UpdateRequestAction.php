b5966bc5b5f60f88bf413b13aaf245366e268fe4
Caedmon Walters <cwalters@umd.edu>
2025-02-19 12:09:42 -0500

    update request

--- src/Application/Actions/Request/UpdateRequestAction.php
+++ src/Application/Actions/Request/UpdateRequestAction.php
@@ -4,20 +4,39 @@ declare(strict_types=1);
 
 namespace App\Application\Actions\Request;
 
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Log\LoggerInterface;
+use App\Services\WorkflowLogger;
+use App\Domain\Approval\ApprovalRepository;
+use App\Domain\Request\RequestRepository;
 use Psr\Http\Message\ResponseInterface as Response;
+
 
 class UpdateRequestAction extends RequestAction
 {
-    /**
-     * {@inheritdoc}
-     */
+    protected ApprovalRepository $approvalRepository;
+    protected LoggerInterface $logger;
+    protected WorkflowLogger $workflowLogger;
+    protected RequestRepository $requestRepository;
+
+    protected ServerRequestInterface $request;
     protected function action(): Response
     {
         $userId = (int) $this->resolveArg('id');
-        $data = $this->getFormData();
-        $request = (array)$this->requestRepository->updateRequestOfId($userId, $data);
-        $this->logger->info("Request id `{$userId}` was updated.");
+        $data =  $this->request->getParsedBody();
+        //$data = $this->getFormData();
+        $decision = $data['decision'];
+        $request = $this->requestRepository->updateRequestOfId($userId, $data);
 
-        return $this->respondWithData($request);
+        $approver = [
+            'request_id' => $request,
+            'approver_id' => $_SESSION['username'],
+            'status' => ucfirst($decision)
+        ];
+        $this->approvalRepository->createApproval($approver);
+        $this->workflowLogger->logAction($request,$_SESSION['username'], 'Submitted',$approver['status']);
+        $this->logger->info("Request ID: `{$request}` updated!");
+
+        return $this->response->withHeader('HX-Redirect', '/thank-you');
     }
 }
